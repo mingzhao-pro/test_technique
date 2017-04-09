a27a0645a5320fe7a0e863bcaba2579a90722a90
Ming ZHAO <mingzhao.pro@gmail.com>
2017-04-09 20:06:13 +0200

    refactor code

--- src/main/scala/Extraction.scala
+++ src/main/scala/Extraction.scala
@@ -1,6 +1,6 @@
 import java.io.{BufferedWriter, File, FileWriter}
-
-import org.scalameter._
+import java.time.{Instant, LocalDateTime, ZoneId}
+import java.time.temporal.ChronoUnit
 
 import scala.collection.mutable
 import scala.io.Source
@@ -10,13 +10,19 @@ import scala.io.Source
   */
 object Extraction {
 
-  def readFile(file: String) = {
-    val lines = Source.fromInputStream(this.getClass.getResourceAsStream(file)).getLines()
-    lines.map(_.split(",")).map(x => ((x(0), x(1)), x(2).toFloat, x(3).toLong)) // (userId, ItemId), rating, time
+  // IntelliJ use .idea/modules as current working directory
+  val FilePathPre = "../../src/main/resources/"
+  val UserIdFile = "lookup_user.csv"
+  val ProductIdFile = "lookup_product.csv"
+  val RatingFile = "agg_ratings.csv"
+
+  def readFile(file: String): Iterator[((String, String), Float, Long)] = {
+    val Splitter = ","
+    Source.fromInputStream(this.getClass.getResourceAsStream(file)).getLines()
+          .map(_.split(Splitter)).map(x => ((x(0), x(1)), x(2).toFloat, x(3).toLong)) // (userId, ItemId), rating, time
   }
 
-  def maxTime(lines : Iterator[((String, String), Float, Long)]): Long = {
-    val lines = readFile("/xag.csv")
+  def getMaxTime(lines : Iterator[((String, String), Float, Long)]): Long = {
     var maxTime = 0L
     for(l <- lines) {
       maxTime = math.max(maxTime, l._3)
@@ -24,25 +30,22 @@ object Extraction {
     maxTime
   }
 
-  def csvPrinter(fileName: String, lines: mutable.Map[String, Integer]) = {
+  def filePrinter(fileName: String, lines: mutable.Map[String, Int]) = {
     val file = new File(fileName)
     val bw = new BufferedWriter(new FileWriter(file))
-    lines.toArray.sortWith((a, b) => a._2 < b._2)foreach(x => {
-      val s = x._1 + "," + x._2.toString
-      bw.write(s)
-      bw.write("\n")
-    })
+    lines.toArray.sortWith((a, b) => a._2 < b._2)
+         .map(x => x._1 + "," + x._2.toString + "\n")
+         .foreach(bw.write)
     bw.close()
   }
 
-  def resultPrinter(fileName: String, lines: mutable.Map[(Integer, Integer), Float]) = {
+  def aggFilePrinter(fileName: String, lines: mutable.Map[(Int, Int), Float]) = {
     val file = new File(fileName)
     val bw = new BufferedWriter(new FileWriter(file))
-    lines.foreach({x =>
-      val s = x._1._1.toString + "," + x._1._2.toString + "," + (math.round(x._2 * 100.0) / 100.0).toString
-      bw.write(s)
-      bw.write("\n")
-    })
+    lines.foreach(x => {
+         val line = x._1._1.toString + "," + x._1._2.toString + "," + (math.round(x._2 * 100.0) / 100.0).toFloat + "\n"
+         bw.write(line)
+       })
     bw.close()
   }
 
@@ -50,39 +53,78 @@ object Extraction {
     * * une pénalité multiplicative de 0.95 est appliquée au rating
     *   pour chaque jour d'écart avec le timestamp maximal de input.csv
     *
-    * @param maxTime maximal timestamp at input.csv
-    * @param time    current rating time
+    * @param nowTime maximal timestamp at input.csv
+    * @param pastTime    current rating time
     * @param rating  original rating
     * @return final rating multiplied by 0.95 for every day interval from the maximal timestamp
     */
-  def finalRating(maxTime: Long, time: Long, rating: Float): Float = {
-    val diffByDay = math.floor((maxTime - time) / 1000 / 60 / 60 / 24) // int <= double
-    (math.pow(0.95, diffByDay) * rating).toFloat
+  def finalRating(nowTime: Long, pastTime: Long, rating: Float): Float = {
+    val now =
+      LocalDateTime.ofInstant(Instant.ofEpochMilli(nowTime), ZoneId.systemDefault())
+
+    val past =
+      LocalDateTime.ofInstant(Instant.ofEpochMilli(pastTime), ZoneId.systemDefault())
+
+    val diff = ChronoUnit.DAYS.between(past, now)
+    (math.pow(0.95, diff) * rating).toFloat
   }
 
-  def main(args: Array[String]): Unit = {
-    val lines = Source.fromInputStream(this.getClass.getResourceAsStream("/xag.csv")).getLines()
-    val splited = lines.map(_.split(",")).map(x => (x(0), x(1), x(2).toFloat, x(3).toLong)) // (userId, ItemId), rating, time
-    val sortedLines = splited.toSeq.sortWith((a, b) => a._4 > b._4)
+  /**
+    *
+    * @param file file to extract
+    */
+  def fileDispatcher(file: String) = {
 
-    //agg_ratings.csv : userIdAsInteger,itemIdAsInteger,ratingSum
-    //utilisateur/produit sont uniques
-    //- userIdAsInteger : identifiant unique d'un utilisateur (Int)
-    //- itemIdAsInteger : identifiant unique d'un produit (Int)
-    //- ratingSum : Somme des ratings pour le couple utilisateur/produit (Float)
+    /**
+      * get idIndice or increment to idIndice and put it to id map
+      * @param id  id in String
+      * @param idIndice id in Int
+      * @param idMap userIdMap or productIdMap
+      * @return (indice for id, max idIndice)
+      */
+    def getIndice(id: String, idIndice: Int, idMap: mutable.Map[String, Int]): (Int, Int) = {
+      idMap.get(id) match {
+        case Some(i) => (i, idIndice)
+        case None => {
+          val indice = idIndice + 1
+          idMap += (id -> indice)
+          (indice, indice)
+        }
+      }
+    }
 
+    // 1. scan the file the find the max time
+    val maxTime = readFile(file).reduce((a, b) => if(a._3 > b._3) a else b)._3
 
+    // 2. apply rating condition, calculate rating and return only valid rating lines
+    val validLines = readFile(file).map(x => (x._1, finalRating(maxTime, x._3, x._2))).filter(_._2 > 0.01)
 
+    // 3. loop file lines, sum ratings by (userId, productId), and combine id_String and id_Int
+    val userIdMap = mutable.Map[String, Int]() // (userId, userIdAsInt)
+    val productIdMap = mutable.Map[String, Int]() // (productId, productIdAsInt)
+    val userProductRatingMap = mutable.Map[(Int, Int), Float]() // (userIdAsInt, productIdAsInt, ratingSum)
 
+    var userIdIndice = -1
+    var productIdIndice = -1
 
-//    // for temp max time, if rating < 0.01, change to 0
-//    val lines = Source.fromInputStream(this.getClass.getResourceAsStream("test.csv")).getLines()
-//    // (timeMax, (userId, itemId, rating, time)
-////    val maxTime = splited.maxBy(_._4)
-//    var maxTime = 0L
-//    val splited = lines.map(_.split(",")).map(x => (x(0), x(1), x(2).toFloat, x(3).toLong)) // (userId, ItemId), rating, time
-//    val maxs = splited.maxBy(_._4)
-//    println("maxTime " + maxTime + " size " + splited.size)
+    validLines.foreach(x => {
+      val userIdString = x._1._1
+      val userId = getIndice(userIdString, userIdIndice, userIdMap)
+      userIdIndice = userId._2
 
+      val productIdString = x._1._2
+      val productId = getIndice(productIdString, productIdIndice, productIdMap)
+      productIdIndice = productId._2
+
+      val key = (userId._1, productId._1)
+      userProductRatingMap.get(key) match {
+        case Some(i) => userProductRatingMap += (key -> (i + x._2))
+        case None => userProductRatingMap += (key -> x._2)
+      }
+    })
+
+    filePrinter(FilePathPre + UserIdFile, userIdMap)
+    filePrinter(FilePathPre + ProductIdFile, productIdMap)
+    aggFilePrinter(FilePathPre + RatingFile, userProductRatingMap)
   }
 }
